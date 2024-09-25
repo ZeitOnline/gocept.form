1a9eac1305e9096a74ca71dffce7e1704b4b3656
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2024-09-25 13:11:46 +0200

    Remove cvs cruft

--- src/gocept/form/action.py
+++ src/gocept/form/action.py
@@ -1,7 +1,6 @@
 # -*- coding: latin-1 -*-
 # Copyright (c) 2007-2008 gocept gmbh & co. kg
 # See also LICENSE.txt
-# $Id$
 """Additional actions."""
 
 import sys
--- src/gocept/form/i18n.py
+++ src/gocept/form/i18n.py
@@ -1,7 +1,5 @@
 # Copyright (c) 2007-2008 gocept gmbh & co. kg
 # See also LICENSE.txt
-# $Id$
-
 import zope.i18nmessageid
 
 
