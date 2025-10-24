6dec20d3bcbc4a4a5da288a257d0e324316af354
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2025-10-24 10:01:29 +0200

    Back to development: 0.10.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name = 'gocept.form',
-    version='0.9.0',
+    version='0.10.0.dev0',
     author = "Christian Zagrodnick",
     author_email = "cz@gocept.com",
     description = "Extensions for zope.formlib",
